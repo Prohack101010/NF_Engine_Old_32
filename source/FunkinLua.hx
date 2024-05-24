eeb9cc52df56554209123871c139895f42c69f9e
Prohack101010 <miracberaber@gmail.com>
2024-05-24 21:34:52 +0300

    Add files via upload

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -1037,23 +1037,23 @@ class FunkinLua {
 			#if android // Extend for check control for android,you can try to extend other key at same way but I'm so lazy. --Write by NF|beihu(北狐丶逐梦)
            if (MusicBeatState.androidc.newhbox != null){ //check for android control and dont check for keyboard
 			    if (variable == 'keys.justPressed.SPACE' && MusicBeatState.androidc.newhbox.buttonSpace.justPressed){
-    			    return LuaUtils.getVarInArray(myClass, variable, allowMaps);
+    			    return FunkinLua.getVarInArray(myClass, variable, allowMaps);
                 }
                 else if (variable == 'keys.pressed.SPACE' && MusicBeatState.androidc.newhbox.buttonSpace.pressed){
-                    return LuaUtils.getVarInArray(myClass, variable, allowMaps);
+                    return FunkinLua.getVarInArray(myClass, variable, allowMaps);
                 }
                 else if (variable == 'keys.justReleased.SPACE' && MusicBeatState.androidc.newhbox.buttonSpace.justReleased){
-                    return LuaUtils.getVarInArray(myClass, variable, allowMaps);
+                    return FunkinLua.getVarInArray(myClass, variable, allowMaps);
                 }
                 
                 if (variable == 'keys.justPressed.SHIFT' && MusicBeatState.androidc.newhbox.buttonShift.justPressed){
-    			    return LuaUtils.getVarInArray(myClass, variable, allowMaps);
+    			    return FunkinLua.getVarInArray(myClass, variable, allowMaps);
                 }
                 else if (variable == 'keys.pressed.SHIFT' && MusicBeatState.androidc.newhbox.buttonShift.pressed){
-                    return LuaUtils.getVarInArray(myClass, variable, allowMaps);
+                    return FunkinLua.getVarInArray(myClass, variable, allowMaps);
                 }
                 else if (variable == 'keys.justReleased.SHIFT' && MusicBeatState.androidc.newhbox.buttonShift.justReleased){
-                    return LuaUtils.getVarInArray(myClass, variable, allowMaps);
+                    return FunkinLua.getVarInArray(myClass, variable, allowMaps);
                 }
            }
            #end
