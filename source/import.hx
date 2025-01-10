0a4af905325eba2c2aef9aba27f57111837e3b26
Prohack101010 <miracberaber@gmail.com>
2025-01-11 00:48:03 +0300

    Fix Imports

--- source/import.hx
+++ source/import.hx
@@ -2,3 +2,20 @@ import Paths;
 
 //Mobile Things
 import mobile.backend.*;
+import android.*;
+import android.flixel.*;
+
+// Android
+#if android
+import android.Tools as AndroidTools;
+import android.Settings as AndroidSettings;
+import android.widget.Toast as AndroidToast;
+import android.content.Context as AndroidContext;
+import android.Permissions as AndroidPermissions;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Environment as AndroidEnvironment;
+import android.os.BatteryManager as AndroidBatteryManager;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+#end
+
+using StringTools;
